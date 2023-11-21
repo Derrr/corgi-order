3a67d34971a9d06b6825a3a9a782cec42630cfe6
trliu <trliu@hillinsight.com>
2023-11-21 15:23:56 +0800

    init

--- corgi-order-service/src/main/java/com/corgi/Application.java
+++ corgi-order-service/src/main/java/com/corgi/Application.java
@@ -3,11 +3,9 @@ package com.corgi;
 import com.alibaba.dubbo.spring.boot.annotation.EnableDubboConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableDubboConfiguration
-@EnableTransactionManagement
 public class Application {
 
     public static void main(String[] args) {
