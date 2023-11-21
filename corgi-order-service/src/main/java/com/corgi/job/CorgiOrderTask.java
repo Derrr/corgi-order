fd9355b4c32bfa341a87529fe7e4ddda572dd911
trliu <trliu@hillinsight.com>
2023-11-21 16:02:03 +0800

    init

--- corgi-order-service/src/main/java/com/corgi/job/CorgiOrderTask.java
+++ corgi-order-service/src/main/java/com/corgi/job/CorgiOrderTask.java
@@ -5,15 +5,9 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.corgi.order.api.CorgiOrderService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import org.springframework.util.CollectionUtils;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 /**
  * @author tairanliu
