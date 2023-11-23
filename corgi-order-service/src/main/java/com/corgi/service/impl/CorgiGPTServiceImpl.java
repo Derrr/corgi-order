a7475e073138f43ce1462f754864bb164bb2635a
trliu <trliu@hillinsight.com>
2023-11-23 17:07:47 +0800

    init

--- corgi-order-service/src/main/java/com/corgi/service/impl/CorgiGPTServiceImpl.java
+++ corgi-order-service/src/main/java/com/corgi/service/impl/CorgiGPTServiceImpl.java
@@ -1,5 +1,12 @@
 package com.corgi.service.impl;
 
+import com.alibaba.dubbo.config.annotation.Service;
+import com.corgi.order.api.CorgiGPTService;
+import com.theokanning.openai.completion.CompletionRequest;
+import com.theokanning.openai.completion.chat.ChatCompletionRequest;
+import com.theokanning.openai.completion.chat.ChatMessage;
+import com.theokanning.openai.image.CreateImageRequest;
+import com.theokanning.openai.service.OpenAiService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
@@ -11,53 +18,52 @@ import java.util.List;
 /**
  * @author tairanliu
  */
-//@Service(interfaceClass = CorgiGPTService.class)
+@Service(interfaceClass = CorgiGPTService.class)
 @Slf4j
 @Component
-public class CorgiGPTServiceImpl {
+public class CorgiGPTServiceImpl implements CorgiGPTService {
 
-
+    @Override
     public String completeChatMessage(String context, String message) {
-//        String token = "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";
-//        OpenAiService service = new OpenAiService(token, Duration.ofSeconds(30));
+        String token = "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";
+        OpenAiService service = new OpenAiService(token, Duration.ofSeconds(30));
 
-//        System.out.println("\nCreating completion...");
-//        CompletionRequest completionRequest = CompletionRequest.builder()
-//                .model("ada")
-//                .prompt("Somebody once told me the world is gonna roll me")
-//                .echo(true)
-//                .user("testing")
-//                .n(3)
-//                .build();
-//        service.createCompletion(completionRequest).getChoices().forEach(System.out::println);
-//
-//        System.out.println("\nCreating Image...");
-//        CreateImageRequest request = CreateImageRequest.builder()
-//                .prompt("A cow breakdancing with a turtle")
-//                .build();
-//
-//        System.out.println("\nImage is located at:");
-//        System.out.println(service.createImage(request).getData().get(0).getUrl());
+        System.out.println("\nCreating completion...");
+        CompletionRequest completionRequest = CompletionRequest.builder()
+                .model("ada")
+                .prompt("Somebody once told me the world is gonna roll me")
+                .echo(true)
+                .user("testing")
+                .n(3)
+                .build();
+        service.createCompletion(completionRequest).getChoices().forEach(System.out::println);
 
-//        System.out.println("Streaming chat completion...");
-//        final List<ChatMessage> messages = new ArrayList<>();
-//        final ChatMessage systemMessage = new ChatMessage("Star", "You are a dog and will speak as such.");
-//        messages.add(systemMessage);
-//        ChatMessage userMessage = new ChatMessage("Admirer", "Hello");
-//        messages.add(userMessage);
-//        ChatCompletionRequest chatCompletionRequest = ChatCompletionRequest
-//                .builder()
-//                .model("gpt-3.5-turbo")
-//                .messages(messages)
-//                .n(1)
-//                .maxTokens(50)
-//                .logitBias(new HashMap<>())
-//                .build();
-//
-//
-//        return service.createChatCompletion(chatCompletionRequest)
-//                .getChoices().get(0).getMessage().getContent();
-        return null;
+        System.out.println("\nCreating Image...");
+        CreateImageRequest request = CreateImageRequest.builder()
+                .prompt("A cow breakdancing with a turtle")
+                .build();
+
+        System.out.println("\nImage is located at:");
+        System.out.println(service.createImage(request).getData().get(0).getUrl());
+
+        System.out.println("Streaming chat completion...");
+        final List<ChatMessage> messages = new ArrayList<>();
+        final ChatMessage systemMessage = new ChatMessage("Star", "You are a dog and will speak as such.");
+        messages.add(systemMessage);
+        ChatMessage userMessage = new ChatMessage("Admirer", "Hello");
+        messages.add(userMessage);
+        ChatCompletionRequest chatCompletionRequest = ChatCompletionRequest
+                .builder()
+                .model("gpt-3.5-turbo")
+                .messages(messages)
+                .n(1)
+                .maxTokens(50)
+                .logitBias(new HashMap<>())
+                .build();
+
+
+        return service.createChatCompletion(chatCompletionRequest)
+                .getChoices().get(0).getMessage().getContent();
     }
 
 
