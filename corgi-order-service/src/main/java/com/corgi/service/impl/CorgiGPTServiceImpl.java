70b0d2d2964822500a82ed34c67e7af694d9804b
trliu <trliu@hillinsight.com>
2023-11-24 10:51:25 +0800

    init

--- corgi-order-service/src/main/java/com/corgi/service/impl/CorgiGPTServiceImpl.java
+++ corgi-order-service/src/main/java/com/corgi/service/impl/CorgiGPTServiceImpl.java
@@ -48,9 +48,9 @@ public class CorgiGPTServiceImpl implements CorgiGPTService {
 
         System.out.println("Streaming chat completion...");
         final List<ChatMessage> messages = new ArrayList<>();
-        final ChatMessage systemMessage = new ChatMessage("Star", "You are a dog and will speak as such.");
+        final ChatMessage systemMessage = new ChatMessage("system", "You are a dog and will speak as such.");
         messages.add(systemMessage);
-        ChatMessage userMessage = new ChatMessage("Admirer", "Hello");
+        ChatMessage userMessage = new ChatMessage("user", "Hello");
         messages.add(userMessage);
         ChatCompletionRequest chatCompletionRequest = ChatCompletionRequest
                 .builder()
